368cdedd8e4aed0f847c4ec25f5727a27d22b0ce
Luke Morrison <lukejamesmorrison@gmail.com>
2020-05-31 13:25:45 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    if(!project.backers.include?(project))
+    if(!project.backers.include?(self))
       project.backers << self
     end
 
